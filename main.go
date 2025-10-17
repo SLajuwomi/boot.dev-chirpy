9ce6c0310e8c88534478a370bd607b0d80b12623
SLajuwomi <stephenlajuwomi@gmail.com>
2025-10-17 00:02:59 -0500

    create server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	newServeMux := http.NewServeMux()
+	newServer := http.Server{
+		Handler: newServeMux,
+		Addr:    ":8080",
+	}
+	err := newServer.ListenAndServe()
+	if err != nil {
+		fmt.Printf("failed to start server: %v\n", err)
+		os.Exit(1)
+	}
 }
